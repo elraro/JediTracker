e21fb9ccb05455ac5dbf65a2873ba0ed7e27d8ee
elraro <elraro@elraro.eu>
2016-05-18 00:18:25 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 17/05/16
+ * Time: 23:24
+ */
---
+++ tracker.php
@@ -0,0 +1,112 @@
+<html>
+<head>
+    <title>JediTracker - www.elraro.eu</title>
+    <meta http-equiv='refresh' content='300'>
+</head>
+<body>
+<style type='text/css' media='screen'>
+    body {
+        font-size: 11px;
+        color: #cdcdcd;
+        background-color: #333333;
+        font-family: Arial
+    }
+
+    td {
+        color: #cdcdcd;
+        font-size: 11px;
+        font-family: Arial
+    }
+
+    .mini10 {
+        color: #cdcdcd;
+        font-size: 11px;
+        font-family: Arial
+    }
+
+    .micro {
+        font-size: 9px;
+        font-family: Verdana:
+        color: #cdcdcd
+    }
+
+    .title {
+        font-size: 11px;
+        font-family: Verdana
+    }
+
+    a:link {
+        color: #cdcdcd
+    }
+
+    a:visited {
+        color: #cdcdcd
+    }
+</style>
+<?php
+    $url = "http://www.elraro.eu:8080/servers/89.36.214.51/29070/"; // TODO esto habrá que cambiarlo
+    $server = file_get_contents($url);
+
+?>
+<div align='center'>
+    <table width='425' border='0' cellspacing='0' cellpadding='1' BGCOLOR='000000'>
+        <tr>
+            <td align='left' valign='middle' BGCOLOR='000000'>
+						<span class='title'>
+							<b><font color='white'><font color=red>J</font><font color=white>K</font><font
+                                        color=red>|</font><font color=white>N</font><font color=red>G</font><font
+                                        color=white>'</font><font color=red>TFFA</font><font color=white>.ESL</font></b>
+						</span>
+            </td>
+            <td align='right' valign='middle' BGCOLOR='000000'>
+                <span class='micro'>89.36.214.51:29070</span>
+            </td>
+        </tr>
+        <tr>
+            <td colspan='2' align='left'>
+                <table width='100%' border='0' cellspacing='0' cellpadding='3'>
+                    <tr>
+                        <td align='middle' valign='top' BGCOLOR='333333' width='135'><img src=map_pics/ffa2.jpg
+                                                                                          height='90' width='120'
+                                                                                          border=1 vspace=2><br><span
+                                class='micro'><a href='http://www.jeditracker.com/' target='_blank'>JediTracker</a><br>by bLaSt<br><font
+                                    color='yellow'><br><a href='http://www.jeditracker.com/' target='_blank'><font
+                                            color='green'><blink>JEDITRACKER</blink></font></a></font></span></td>
+                        <td align='left' valign='top' BGCOLOR='333333'>
+                            <b>Tipo de Juego: <span class='title'>Team FFA</span></b><br>
+                            <b>Mapa Actual:</b> &quot;mp/ffa2&quot;<br>
+                            <b>Limite de Muertes:</b> 0<br>
+                            <b>Limite de Tiempo:</b> 15 mins.
+                            <p>
+                                <b><span class='micro'>Ejecutando el MOD:</b> <i>basejka</i></span><br>
+                                <b><span class='micro'>Version:</b> <i>JAmp: v1.0.1.1 linux-i386 Nov 10 2003</i></span>
+                                <br>
+                                <b><span class='micro'>Jugadores Conectados:</b> <i>0/12</i></span>
+                                <div align='center'>
+                            <p><span class='micro'><a href='javascript:void(0)'
+                                                      onClick=javascript:window.open('online.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','Players','toolbar=no,width=280,height=375,scrollbars=yes')>&iquest;Quien Esta en Linea?</a></span>
+                            </p>
+</div>
+</td>
+<td align='right' valign='top' BGCOLOR='333333' width='30'>
+    <a href='javascript:void(0)'
+       onClick=javascript:window.open('console.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','RCON','toolbar=no,width=400,height=250')><span
+            class='micro'><b>rcon</b></span></a>
+    <a href='javascript:void(0)'
+       onClick=javascript:window.open('cvars.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','CVARS','toolbar=no,width=420,height=450,scrollbars=yes')><span
+            class='micro'><b>cvars</b></span></a>
+    <a href='javascript:void(0)'
+       onClick=javascript:window.open('force.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','FORCE','toolbar=no,width=360,height=450,scrollbars=yes')><span
+            class='micro'><b>force</b></span></a>
+    <a href='javascript:void(0)'
+       onClick=javascript:window.open('weapons.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','WEAPONS','toolbar=no,width=360,height=500,scrollbars=yes')><span
+            class='micro'><b>weaps</b></span></a>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</div>
+</body>
+</html>
