5409af351f55f24ccf3b2405c8d785b7ff171de7
elraro <elraro@elraro.eu>
2016-05-19 23:44:15 +0200

    Seguimos aprendiendo 2

--- tracker.php
+++ tracker.php
@@ -1,4 +1,17 @@
 <?php
+if(isset($_GET["ipaddress"]) && isset($_GET["port"])) {
+    $ip = $_GET["ipaddress"];
+    $port = $_GET["port"];
+} else {
+    die();
+}
+
+$url = "http://www.elraro.eu:8080/servers/" . $ip . "/" . $port . "/";
+//echo $url;
+$json = file_get_contents($url);
+$server = json_decode($json);
+print_r($server);
+echo $server["status"];
 ?>
 <html>
 <head>
@@ -45,12 +58,6 @@
         color: #cdcdcd
     }
 </style>
-<?php
-$url = "http://www.elraro.eu:8080/servers/89.36.214.51/29070/"; // TODO esto habrá que cambiarlo
-$json = file_get_contents($url);
-$server = json_decode($json, true);
-print_r($server);
-?>
 <div align='center'>
     <table width='425' border='0' cellspacing='0' cellpadding='1' BGCOLOR='000000'>
         <tr>
@@ -70,12 +77,12 @@ print_r($server);
                 <table width='100%' border='0' cellspacing='0' cellpadding='3'>
                     <tr>
                         <td align='middle' valign='top' BGCOLOR='333333' width='135'>
-                            <img src=map_pics/ffa2.jpg height='90' width='120' border=1 vspace=2>
+                            <img src="<?php echo "map_pics/" . $server["parameters"]["mapname"] . ".jpg" ?>" height='90' width='120' border=1 vspace=2>
                             <br>
                             <span class='micro'>
-                                <a href='http://www.jeditracker.com/' target='_blank'>JediTracker</a>
-                                <br>by bLaSt<br>
-                                <br><a href='http://www.jeditracker.com/' target='_blank'><font
+                                <a href='http://jk.elraro.eu/' target='_blank'>JediTracker</a>
+                                <br>by elraro<br>
+                                <br><a href='http://jk.elraro.eu/' target='_blank'><font
                                         color='green'>JEDITRACKER</font></a>
                             </span>
                         </td>
@@ -104,22 +111,20 @@ print_r($server);
                                         default:
                                             echo "FFA";
                                     }
-
-
                                     ?></span></b><br>
-                            <b>Mapa Actual:</b> &quot;mp/ffa2&quot;<br>
-                            <b>Limite de Muertes:</b> 0<br>
-                            <b>Limite de Tiempo:</b> 15 mins.
+                            <b>Mapa Actual:</b> &quot;<?php echo $server["parameters"]["mapname"]; ?>&quot;<br>
+                            <b>Limite de Muertes:</b> <?php echo $server["parameters"]["fraglimit"]; ?><br>
+                            <b>Limite de Tiempo:</b> <?php echo $server["parameters"]["timelimit"]; ?> mins.
                             <div style="margin-top: 10px;">
-                                <b><span class='micro'>Ejecutando el MOD:</span></b> <span class='micro'><i>basejka</i></span><br>
-                                <b><span class='micro'>Version:</span></b> <span class='micro'><i>JAmp: v1.0.1.1 linux-i386 Nov 10 2003</i></span>
+                                <b><span class='micro'>Ejecutando el MOD:</span></b> <span class='micro'><i><?php echo $server["parameters"]["gamename"]; ?></i></span><br>
+                                <b><span class='micro'>Version:</span></b> <span class='micro'><i><?php echo $server["parameters"]["version"]; ?></i></span>
                                 <br>
-                                <b><span class='micro'>Jugadores Conectados:</span></b> <span class='micro'><i>0/12</i></span>
+                                <b><span class='micro'>Jugadores Conectados:</span></b> <span class='micro'><i><?php echo $server["currentClients"] . "/" . $server["maxClients"]?></i></span>
                                 <div align='center'>
                                     <p>
                                         <span class='micro'>
                                             <a href='javascript:void(0)'
-                                               onClick=javascript:window.open('online.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','Players','toolbar=no,width=280,height=375,scrollbars=yes')>&iquest;Quien Esta en Linea?</a>
+                                               onClick=javascript:window.open('online.php?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','Players','toolbar=no,width=280,height=375,scrollbars=yes')>&iquest;Quien Esta en Linea?</a>
                                         </span>
                                     </p>
                                 </div>
@@ -127,19 +132,19 @@ print_r($server);
                         </td>
                         <td align='right' valign='top' BGCOLOR='333333' width='30'>
                             <a href='javascript:void(0)'
-                               onClick=javascript:window.open('console.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','RCON','toolbar=no,width=400,height=250')>
+                               onClick=javascript:window.open('console.php?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','RCON','toolbar=no,width=400,height=250')>
                                 <span class='micro'><b>rcon</b></span>
                             </a>
                             <a href='javascript:void(0)'
-                               onClick=javascript:window.open('cvars.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','CVARS','toolbar=no,width=420,height=450,scrollbars=yes')>
+                               onClick=javascript:window.open('cvars.php?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','CVARS','toolbar=no,width=420,height=450,scrollbars=yes')>
                                 <span class='micro'><b>cvars</b></span>
                             </a>
                             <a href='javascript:void(0)'
-                               onClick=javascript:window.open('force.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','FORCE','toolbar=no,width=360,height=450,scrollbars=yes')>
+                               onClick=javascript:window.open('force.php?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','FORCE','toolbar=no,width=360,height=450,scrollbars=yes')>
                                 <span class='micro'><b>force</b></span>
                             </a>
                             <a href='javascript:void(0)'
-                               onClick=javascript:window.open('weapons.pl?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','WEAPONS','toolbar=no,width=360,height=500,scrollbars=yes')>
+                               onClick=javascript:window.open('weapons.php?ipaddress=89.36.214.51&port=29070&bgcolor=333333&titlebar=000000&display=cdcdcd&window=333333','WEAPONS','toolbar=no,width=360,height=500,scrollbars=yes')>
                                 <span class='micro'><b>weaps</b></span>
                             </a>
                         </td>
