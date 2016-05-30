940740ff7f357b43eab3c554c52b33782fd52f06
elraro <elraro@elraro.eu>
2016-05-30 23:26:49 +0200

    Test bit field

--- weapons.php
+++ weapons.php
@@ -1,3 +1,28 @@
 <?php
 
+/**
+ * Shows all active bits
+ *
+ * @param int $flag
+ * @return array
+ */
+function bits($flag)
+{
+    $setBits = array();
+    for ($i = 0; $i <= 31; $i++) {
+        if ($flag & (1 << $i)) {
+            $setBits[] = (1 << $i);
+        }
+    }
+
+    // Sort array to order the bits
+    sort($setBits);
+
+    return $setBits;
+}
+
+echo "<pre>";
+var_dump(bits(524287));
+echo "</pre>";
+
 ?>
